d5de42e9ad46026c7dc3b40387a9404f211b8ef5
Nico Smith <101956935+niiccoo2@users.noreply.github.com>
2024-09-30 13:26:13 +0000

    added .csv

--- main.py
+++ main.py
@@ -1,1 +1,6 @@
-print("Hello CodeSandbox!")
+# https://developers.google.com/forms/api/reference/rest/v1/forms.responses
+import csv
+with open('eggs.csv', newline='') as csvfile:
+    spamreader = csv.reader(csvfile, delimiter=' ', quotechar='|')
+    for row in spamreader:
+        print(', '.join(row))

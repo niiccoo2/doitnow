1ee3680d3ed5471ceaf6a5b9857170314c735321
babychicken11 <131915070+babychicken11@users.noreply.github.com>
2024-12-05 18:48:33 +0000

    testing (it didnt work)

--- main.py
+++ main.py
@@ -25,7 +25,7 @@ date=0
 email=1
 class_color=2
 name=3
-pickfile = 2 # 1 = test, 2 = 7/8, 3 = 6th
+pickfile = 1 # 1 = test, 2 = 7/8, 3 = 6th
 
 if pickfile==1:
     file_name = 'Fai Adesso - Form Responses TEST.csv' # Test
